a50481331b193c4d1f74e9d84c1af212e5157985
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-06-04 18:24:09 -0300

    fix: Change a component to Link component

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import Link from "next/link";
 
 export function Hero() {
   return (
@@ -15,9 +16,9 @@ export function Hero() {
         </p>
       </div>
 
-      <a href="" className='inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600'>
+      <Link href="/memories/new" className='inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600'>
         CADASTRAR LEMBRANÇA
-      </a>
+      </Link>
     </div>
   )
 }
